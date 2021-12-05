4dde325d52014334d061cc837ac6943f2afffacc
Valeriy Umarov <valeriy.umarov@ya.ru>
2021-12-05 18:22:41 +0300

    ‘your_comment’

---
+++ js/index.js
@@ -0,0 +1,29 @@
+document.getElementsByName("btn")[0].onclick = function(){window.location.href='about.html'}
+document.getElementsByName("meet")[0].onclick = function(){window.location.href='meeting.html'}
+document.getElementsByName("task")[0].onclick = function(){window.location.href='task.html'}
+document.getElementsByName("contracts")[0].onclick = function(){window.location.href='contracts.html'}
+document.getElementsByName("analytics")[0].onclick = function(){window.location.href='analytics.html'}
+document.getElementsByName("osn")[0].onclick = function(){window.location.href='osn.html'}
+document.getElementsByName("osz")[0].onclick = function(){window.location.href='osz.html'}
+document.getElementsByName("pto")[0].onclick = function(){window.location.href='pto.html'}
+document.getElementsByName("depart")[0].onclick = function(){window.location.href='depart.html'}
+
+
+// window.addEventListener("resize", AutoScale);
+// AutoScale()
+// function AutoScale() {
+//     let width = window.innerWidth;
+
+//     if(width > 1280)
+//     {
+//    	ChangeScale("big");
+//     }
+//     else if(width <= 1280 && width > 720)
+//     {
+//    	ChangeScale("normal");
+//     }
+//     else if(width < 720)
+//     {
+//    	ChangeScale("small");
+//     }
+// };
